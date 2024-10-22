a455d78574653ec948ca98327d58d5c1441b5714
e1b22076 <e1b22076@oit.ac.jp>
2024-10-22 14:59:30 +0900

    5-3

--- src/main/java/oit/is/z2636/dai5/team4/team45/model/FruitMapper.java
+++ src/main/java/oit/is/z2636/dai5/team4/team45/model/FruitMapper.java
@@ -4,9 +4,17 @@ import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Delete;
 
 @Mapper
 public interface FruitMapper {
   @Select("SELECT * FROM fruit;")
   ArrayList<Fruit> selectAllFruit();
+
+  @Delete("DELETE FROM FRUIT WHERE Id= #{id}")
+  boolean deleteById(int id);
+
+  @Select("SELECT * FROM fruit WHERE Id=#{id};")
+  Fruit selectById(int id);
+
 }
