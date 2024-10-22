a9f65982d158090865d8e05e791bc79df3594749
e1b22080 <e1b22080@oit.ac.jp>
2024-10-22 13:52:53 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z2636/dai5/team4/team45/Team45Application.java
@@ -0,0 +1,13 @@
+package oit.is.z2636.dai5.team4.team45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team45Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z2636/dai5/team4/team45/Team45ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z2636.dai5.team4.team45;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
